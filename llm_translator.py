b3fa94d18aceb8dfded400a1554c0ec97fb396fe
josephrajasekar-maker <josephrajasekar@googlemail.com>
2026-03-12 23:26:31 +0530

    Update llm_translator.py

--- llm_translator.py
+++ llm_translator.py
@@ -9,16 +9,24 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def translate_cobol_to_python(cobol_code):
 
-    prompt = COBOL_TO_PYTHON_PROMPT.format(
-        cobol_code=cobol_code
-    )
+    prompt = f"""
+Convert the following COBOL program to Python.
 
-    response = client.chat.completions.create(
-        model="gpt-4.1",
-        messages=[
-            {"role": "user", "content": prompt}
-        ],
-        temperature=0
-    )
+COBOL CODE:
+{cobol_code}
+"""
+    try:
 
+         response = client.chat.completions.create(
+            model="gpt-4.1",
+            messages=[
+            {"role": "user", "content": prompt},
+            temperature=0    
+         ],
+          
     return response.choices[0].message.content
+
+    except Exception as e:
+
+        print("AI translation failed:", e)
+        return "# AI translation failed"
