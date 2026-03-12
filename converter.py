0d264f8424c10c108b558721e8dcc981d752a204
josephrajasekar-maker <josephrajasekar@googlemail.com>
2026-03-12 23:17:45 +0530

    Update converter.py

--- converter.py
+++ converter.py
@@ -9,20 +9,20 @@ def convert_program(input_file):
     print("Reading COBOL program...")
 
     cobol_code = read_cobol_file(input_file)
-
+    print("COBOL length:", len(cobol_code))
     print("Sending to AI for conversion...")
 
     python_code = translate_cobol_to_python(cobol_code)
-    print(python_code)
+    print("Python code received from AI")
     output_file = input_file.replace(".cbl", ".py")
-
+     
     output_path = os.path.join("output", output_file)
-
+    os.makedirs("output", exist_ok=True)
     with open(output_path, "w") as f:
         f.write(python_code)
 
-    print("Python program generated:")
-    print(output_path)
+    print("Python program generated:",output_path )
+    #print(output_path)
 
 
 def main():
@@ -30,7 +30,7 @@ def main():
     cobol_program = "sample_cobol/customer_batch.cbl"
 
     convert_program(cobol_program)
-    print(cobol_program)
+    #print(cobol_program)
 
 if __name__ == "__main__":
     main()
